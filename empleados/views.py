0b834a331d9a88c1ee9a0dfbf9c673196f0c36de
enridami <enri124589@gmail.com>
2026-04-20 14:01:33 -0300

    config views

--- empleados/views.py
+++ empleados/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Empleado
+from .serializers import EmpleadoSerializer
 
-# Create your views here.
+
+"""Encargada de listar y crear objetos de tipo Empleado"""
+class EmpleadoListCreateView(generics.ListCreateAPIView):
+    queryset = Empleado.objects.all().order_by('idEmpleado')
+    serializer_class = EmpleadoSerializer
+
+
+"""Encargada de recuperar, actualizar o eliminar un empleado en particular"""
+class EmpleadoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Empleado.objects.all()
+    serializer_class = EmpleadoSerializer
+    lookup_field = 'idEmpleado'
